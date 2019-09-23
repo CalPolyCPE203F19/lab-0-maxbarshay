37319232d77638842cab4ebeccbd9e13cf5b9cdd
Max Barshay <mbarshay@calpoly.edu>
2019-09-23 13:06:41 -0700

    First commit

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,36 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
+        
+        System.out.println("x: " + x + " y: " + y + " z: " + z);
+        
+        int[] nums = {3, 6, -1, 2};
 
-   
+        for (int i = 0; i < nums.length; i++)
+            System.out.println(nums[i]);
+
+        // numFound = char_count(y, "l");
+        // System.out.println("Found: " + numFound);
+
+        for (int i = 0; i < 11; i++)
+            System.out.print(i + " ");
+
+    }
+
+    // public static int charCount(String s, String c)
+    // {
+    //     int count = 0;
+    //     for (String ch : s)
+    //     {
+    //         if (ch == c)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    // }
+
+
    }
-}
